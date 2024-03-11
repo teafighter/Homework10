42f160a9049b197c8743f23dfde1afd81c90d4d2
KIRILL <teafighter@gmail.com>
2024-03-11 17:07:44 +0300

    Привет, проверь плиз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Задача 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        // Задача 2
+        String fullNameUpperCase = fullName.toUpperCase(); // имеет ли смысл заводить под это дело отдельную переменную?
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullNameUpperCase);
+
+        // Задача 3
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace('ё', 'е');
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName);
+
     }
 }
